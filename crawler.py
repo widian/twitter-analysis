bd616381864bbd7532279c273e6965d184116c4f
Lee <widian@kaist.ac.kr>
2015-10-06 17:19:45 +0900

    ! crawler가 더이상 TwitterSearch를 사용하지 않도록 변경

--- crawler.py
+++ crawler.py
@@ -14,46 +14,43 @@ def crawling_tweet_user():
     from application.util import PrintString
     ps = PrintString()
     try:
-        tss = TweetSearchSupport()
-        ts = tss.get_ts()
-        today = datetime.datetime.now().date()
-        tso = tss.generate_user_order("lys2419", today)
-        count = 0
-        foreign_tweet_counter = 0
-        hash_tags = set()
-        processor = TwitterKoreanProcessor()
-        for tweet in ts.search_tweets_iterable(tso):
-            tweet_text = ('%s %s @%s tweeted: %s' % (tweet['id'], tweet['created_at'], tweet['user']['screen_name'], tweet['text']))
-            print tweet_text, tweet
-            tokens = processor.tokenize(tweet['text'])
-            for token in tokens:
-                # Tokenize된 Token들에서 정보 추출하는 부분.
-                foreign_flag = False
-                if token.pos == 'Foreign':
-                    if not foreign_flag:
-                        foreign_tweet_counter += 1
-                        foreign_flag = True
-                elif token.pos == 'Hashtag':
-                    hash_tags.add(token.text.encode('utf-8'))
-            ps.print_tokens(tokens)
-            count += 1
-            break
-        return True
-    except TwitterSearchException as e:
+        ts = TweetSupport()
+        api = ts.get_api()
+        try:
+            minimal_max_id = None
+            while True:
+                statuses = api.GetSearch(
+                        term=u"미오",
+                        count=3000,
+                        max_id=minimal_max_id,
+                        include_entities=True)
+                minimal_max_id = None
+                for tweet in statuses:
+                    tweet_text = ('%s %s @%s tweeted: %s' % (tweet.id, tweet.created_at, tweet.GetUser().screen_name, tweet.text))
+                    minimal_max_id = tweet.id
+                    print tweet_text 
+                if minimal_max_id is None:
+                    break
+                else:
+                    minimal_max_id -= 1
+        except TwitterError as f:
+            print f
+            return True
+    except TwitterError as e:
         print e
-        return True
 
 def get_followers():
     try:
         ts = TweetSupport()
         api = ts.get_api()
-        follower_ids = api.GetFollowerIDs(screen_name='ancom21c')
-        for follower in follower_ids:
-            try:
-                print "------------%s-----------" % (follower)
-                print api.GetUser(user_id=follower).GetDescription()
-            except TwitterError as api_error:
-                print api_error
+        follower_ids = api.GetFollowerIDs(screen_name='saenuridang')
+        print len(follower_ids)
+#        for follower in follower_ids:
+#            try:
+#                print "------------%s-----------" % (follower)
+#                print api.GetUser(user_id=follower).GetDescription()
+#            except TwitterError as api_error:
+#                print api_error
     except TwitterError as e:
         print e
 
@@ -64,7 +61,17 @@ def get_rate_limit_status():
         print api.GetRateLimitStatus()
     except TwitterError as e:
         print e
+
+def get_user_info(screen_name):
+    try:
+        ts = TweetSupport()
+        api = ts.get_api()
+        print api.GetUser(screen_name=screen_name)
+    except TwitterError as e:
+        print e
 if __name__ == "__main__":
 #    crawling_tweet_user()
 #    get_followers()
-    get_rate_limit_status()
+#    get_rate_limit_status()
+#    get_user_info('lys2419')
+#    print 'do nothing'
