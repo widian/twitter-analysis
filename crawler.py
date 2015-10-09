44d3824212de9a618beaf6508001fd925ab9d4bf
Lee <widian@kaist.ac.kr>
2015-10-09 13:46:06 +0900

    ! crawler 수정, model에 RateLimit의 column name변경

--- crawler.py
+++ crawler.py
@@ -30,7 +30,9 @@ class Crawler(object):
         myre = None
         try:
             # Wide UCS-4 build
+#            \U0001f1f0\U0001f1f7
             myre = re.compile(u'['
+                    u'\U0001F1E6-\U0001F1FF'
                     u'\U0001F300-\U0001F64F'
                     u'\U0001F680-\U0001F6FF'
                     u'\u2600-\u26FF\u2700-\u27BF]+', 
@@ -38,7 +40,7 @@ class Crawler(object):
         except re.error:
             # Narrow UCS-2 build
             myre = re.compile(u'('
-                    u'\ud83c[\udf00-\udfff]|'
+                    u'\ud83c[\udde6-\uddff\udf00-\udfff]|'
                     u'\ud83d[\udc00-\ude4f\ude80-\udeff]|'
                     u'[\u2600-\u26FF\u2700-\u27BF])+', 
                     re.UNICODE)
@@ -47,7 +49,7 @@ class Crawler(object):
 
     def get_rate_limit_status(self):
         try:
-            print self.api.GetRateLimitStatus()
+            return self.api.GetRateLimitStatus()
         except TwitterError as e:
             # NOTE NOTE NOTE NOTE
             # 아마도 RateLimitStatus까지 RateLimit에 걸린 상황에 해당 루틴으로 들어올 가능성 높음
@@ -56,10 +58,18 @@ class Crawler(object):
 
     def rate_limit_handler(self, case, 
             process_name=None, 
-            minimal_max_id=None, 
-            limit_since=None):
+            minimum_max_id=None):
+        rate_limit = self.get_rate_limit_status()
+        # DEBUG process_name : /statuses/user_timeline
+        # TODO : Need to parse from process_name to inner dictionary iterator
+        items = process_name.split('/')
+        target_dict = rate_limit['resources']
+        for item in items[1:]:
+            target_dict = target_dict[item]
+        # print result > {u'reset': 1444362152, u'limit': 180, u'remaining': 0}
+        limit_since = datetime.datetime.fromtimestamp(int(target_dict['reset']))
         sess = Session()
-        sess.add(RateLimit(limit_since, process_name, minimal_max_id=minimal_max_id))
+        sess.add(RateLimit(limit_since, process_name, minimum_max_id=minimum_max_id))
         sess.commit()
         sess.close()
         print 'rate limit!'
@@ -73,7 +83,8 @@ class Crawler(object):
         return dt - datetime.timedelta(seconds=time_tuple[-1])
    
 class UserTimelineCrawler(Crawler):
-    minimal_max_id = None
+    minimum_max_id = None
+    process_name = '/statuses/user_timeline'
     def __init__(self):
         Crawler.__init__(self)
     
@@ -81,39 +92,59 @@ class UserTimelineCrawler(Crawler):
         sess = None
         try:
             sess = Session()
-            cached_max_id_row = sess.query(func.min(Tweet.id)).first()
-            if cached_max_id_row[0] is not None:
-                self.minimal_max_id = cached_max_id_row[0] - 1
+
             while True:
                 statuses = self.api.GetUserTimeline(
                         screen_name=username,
-                        max_id=self.minimal_max_id)
-                self.minimal_max_id = None
+                        max_id=self.minimum_max_id)
+                self.minimum_max_id = None
+                target_user_id = None
                 for tweet in statuses:
-                    tweet_text = ('%s %s @%s tweeted: %s' % (tweet.id, tweet.created_at, tweet.GetUser().screen_name, tweet.text))
-                    self.minimal_max_id = tweet.id
+                    target_user_id = tweet.user.id
+
+                    self.minimum_max_id = tweet.id
                     exist = sess.query(Tweet).filter(Tweet.id==tweet.id).first()
                     if not exist:
-                        sess.add(Tweet(
+                        # Make Data Row for add to table
+                        tweet_chunk = Tweet(
                             tweet.id,
                             self.parse_ignore(tweet.text),
-                            tweet.GetUser().id,
-                            self.to_datetime(tweet.created_at)))
-                    sess.commit()
-                    sess.close()
-                    print tweet_text 
+                            tweet.user.id,
+                            self.to_datetime(tweet.created_at))
+                        if tweet.retweeted_status:
+                            tweet_chunk.retweet_owner = tweet.retweeted_status.user.id
+                            tweet_chunk.retweet_origin = tweet.retweeted_status.id
+                        if tweet.in_reply_to_user_id:
+                            tweet_chunk.reply_to = tweet.in_reply_to_user_id
+                        sess.add(tweet_chunk)
 
-                if self.minimal_max_id is None:
+                    # print tweet search result
+                    tweet_text = ('%s %s @%s tweeted: %s' % (tweet.id, tweet.created_at, tweet.GetUser().screen_name, tweet.text))
+                    print tweet_text 
+                if self.minimum_max_id is None:
                     break
                 else:
-                    self.minimal_max_id -= 1
+                    """ 만약 캐시된 tweet_id의 maximum이 이번 검색에서 얻은 minimum_id보다 작다면
+                    """
+                    cached_row = sess.query(func.max(Tweet.id)).filter(Tweet.user == target_user_id).first()
+                    if cached_row[0] is not None:
+                        """ cache에서 가지고 있는 minimum id를 새 minimum_max_id로 지정해줌. 
+                        """
+                        maximum_cached_id = cached_row[0]
+                        if maximum_cached_id > self.minimum_max_id:
+                            cached_row = sess.query(func.min(Tweet.id)).filter(Tweet.user == target_user_id).first()
+                            self.minimum_max_id = cached_row[0]
+                    """ 최종에는 - 1
+                    """
+                    self.minimum_max_id -= 1
+
             sess.commit()
             sess.close()
             return True
         except TwitterError as e:
             t = TweetErrorHandler(e)
             t.add_handler(88, self.rate_limit_handler)
-            t.invoke(process_name='user_tweet', minimal_max_id=8, limit_since=datetime.datetime.now())
+            t.invoke(process_name=self.process_name, minimum_max_id=self.minimum_max_id)
             if sess is not None:
                 sess.commit()
                 sess.close()
@@ -124,22 +155,22 @@ if __name__ == "__main__":
         try:
             ts = TweetSupport()
             api = ts.get_api()
-            minimal_max_id = None
+            minimum_max_id = None
             while True:
                 statuses = api.GetSearch(
                         term=u"미오",
                         count=3000,
-                        max_id=minimal_max_id,
+                        max_id=minimum_max_id,
                         include_entities=True)
-                minimal_max_id = None
+                minimum_max_id = None
                 for tweet in statuses:
                     tweet_text = ('%s %s @%s tweeted: %s' % (tweet.id, tweet.created_at, tweet.GetUser().screen_name, tweet.text))
-                    minimal_max_id = tweet.id
+                    minimum_max_id = tweet.id
                     print tweet_text 
-                if minimal_max_id is None:
+                if minimum_max_id is None:
                     break
                 else:
-                    minimal_max_id -= 1
+                    minimum_max_id -= 1
             return True
         except TwitterError as e:
             print e
@@ -158,7 +189,14 @@ if __name__ == "__main__":
     #            except TwitterError as api_error:
     #                print api_error
         except TwitterError as e:
+            t = TweetErrorHandler(e)
+            t.add_handler(88, self.rate_limit_handler)
+            t.invoke(process_name='/followers/ids')
+            if sess is not None:
+                sess.commit()
+                sess.close()
             print e
+            return False
 
     def get_rate_limit_status():
         try:
@@ -179,4 +217,5 @@ if __name__ == "__main__":
 #    get_followers()
 #    get_rate_limit_status()
 #    get_user_info('lys2419')
-    UserTimelineCrawler().crawling('twittlions')
+    UserTimelineCrawler().crawling('doosanbears1982')
+
--- support/model.py
+++ support/model.py
@@ -19,11 +19,12 @@ class Tweet(Base):
 
     id = Column(BigInteger, primary_key=True)
     text = Column(String(140), nullable=False)
-    user = Column(Integer, nullable=False)
-    retweeted = Column(Integer)
+    user = Column(BigInteger, nullable=False)
+    retweet_owner = Column(BigInteger)
     retweet_origin = Column(BigInteger)
     created_at = Column(DateTime, nullable=False)
     collected_date = Column(DateTime, nullable=False)
+    reply_to = Column(BigInteger)
 
 class User(Base):
     __tablename__ = 'user'
@@ -42,15 +43,15 @@ class User(Base):
 class RateLimit(Base):
     __tablename__ = 'rate_limit'
 
-    def __init__(self, limit, process_name, minimal_max_id=None):
+    def __init__(self, limit, process_name, minimum_max_id=None):
         self.limit = limit
         self.process_name = process_name
-        self.minimal_max_id = minimal_max_id
+        self.minimum_max_id = minimum_max_id 
         self.collected_date = datetime.datetime.now()
 
     id = Column(Integer, primary_key=True)
     limit = Column(DateTime, nullable=False)
     collected_date = Column(DateTime, nullable=False)
     process_name = Column(String(32), nullable=False)
-    minimal_max_id = Column(BigInteger)
+    minimum_max_id = Column(BigInteger)
 
