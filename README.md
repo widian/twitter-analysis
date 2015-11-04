b8386b928f2a7ac6663a0250e460e607505765fc
Lee <widian@kaist.ac.kr>
2015-11-04 16:36:00 +0900

    ! analyzer에 한국인 구분 유무 루틴을 시험적으로추가

--- README.md
+++ README.md
@@ -4,9 +4,9 @@
 - 그 다음, ipython shell에 %pylab을 기입
 - 그 다음, %run $script-이름을 기입해서 실행
 
-> Example
-> ipython
-> In[1] : %pylab
+### Example
+> $ ipython  
+> In[1] : %pylab  
 > In[2] : %run reference.py
 
 이렇게 하면 matplotlib 창이 하나 뜨게 되고, 결과를 확인할 수 있다.
--- analyzer.py
+++ analyzer.py
@@ -3,12 +3,13 @@
 
 from __future__ import print_function
 
-from support.mysql_support import Session
-from support.model import Tweet
 from collections import OrderedDict
 
 # NLP Processors
 from twkorean import TwitterKoreanProcessor
+
+from support.mysql_support import Session
+from support.model import Tweet, User
 
 def analyze(user_id):
     sess = Session()
@@ -32,6 +33,45 @@ def analyze(user_id):
         ps.print_tokens(tokens)
         print(noun_counter)
     return noun_usage_dict, noun_counter
+
+def new_analyze():
+    sess = Session()
+    ps = PrintString()
+
+    processor = TwitterKoreanProcessor(stemming=False)
+
+    users = sess.query(User)\
+                .all()
+
+    for user in users:
+        user_id = user.id
+        noun_usage_dict = OrderedDict()
+        pos_set = set([])
+        tweet_counter = 0
+        tweets = sess.query(Tweet)\
+                 .filter(Tweet.user == user_id)\
+                 .limit(200)\
+                 .all()
+
+        for tweet in tweets:
+            tweet_counter += 1
+            tokens = processor.tokenize(tweet.text)
+            for token in tokens:
+                pos_set.add(token.pos)
+                if token.pos == 'Noun':
+                    if token.text in noun_usage_dict:
+                        noun_usage_dict[token.text] += 1
+                    else:
+                        noun_usage_dict[token.text] = 1
+        if len(tweets) < 200:
+            print(("%d is Unknown User. Tweet Count : %d") % (user_id, len(tweets)))
+        else:
+            if 'Noun' not in pos_set:
+                print(("%d is Foreigner User") % user_id)
+            else:
+                print(("%d is Korean User") % user_id)
+        #print(tweet_counter, pos_set)
+    return True
 
 class PrintString(object):
     def print_tokens(self, tokens, end="\n"):
@@ -59,12 +99,12 @@ class PrintString(object):
             print(")", end=end)
 
 if __name__ == '__main__':
-    noun_usage_result = analyze(68585222)
-    noun_usage_dict = noun_usage_result[0]
-    noun_count = noun_usage_result[1]
-    for item in OrderedDict(sorted(noun_usage_dict.items(), key = lambda t:t[1], reverse=True)).items():
-        print(item[0], item[1], "ratio : ", item[1] / float(noun_count))
-
+#    noun_usage_result = analyze(68585222)
+#    noun_usage_dict = noun_usage_result[0]
+#    noun_count = noun_usage_result[1]
+#    for item in OrderedDict(sorted(noun_usage_dict.items(), key = lambda t:t[1], reverse=True)).items():
+#        print(item[0], item[1], "ratio : ", item[1] / float(noun_count))
+        
 #    processor = TwitterKoreanProcessor(stemming=False)
 #    text = u"나바로가 홈런을 쳐서 삼성이 4:0으로 승리했다 쩔엌ㅋㅋㅋㅋㅋ" 
 #    text = u"대신증권사에서 마이다스미소중소형주증권투자신탁상품을 가입했어요"
@@ -73,4 +113,4 @@ if __name__ == '__main__':
 #    ps = PrintString()
 #    ps.print_tokens(tokens)
 #
-    
+    new_analyze()
